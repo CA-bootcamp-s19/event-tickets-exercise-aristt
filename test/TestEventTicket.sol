3c96142309c6a8fe87ad131a0371cca461b8a8c0
Aristide Piazza <APIAZZA00@it.ema.ad.pwcinternal.com>
2019-06-27 12:39:22 +0200

    solidity test

--- test/TestEventTicket.sol
+++ test/TestEventTicket.sol
@@ -7,6 +7,20 @@ import "../contracts/EventTicketsV2.sol";
 
 contract TestEventTicket {
 
+  string _description = "description";
+  string _url = "URL";
+  uint _ticketNumber = 100;
 
+  function testEventDetails() public {
+    EventTickets eventTickets = EventTickets(DeployedAddresses.EventTickets());
+
+    (string memory description, string memory website, uint totalTickets, uint sales, ) = eventTickets.readEvent();
+
+    Assert.equal(description, _description, "the event descriptions should match");
+    Assert.equal(website, _url, "the event urls should match");
+    Assert.equal(totalTickets, _ticketNumber, "the number of tickets for sale should be set");
+    Assert.equal(sales, 0, "the ticket sales should be 0");
+
+  }
 
 }
